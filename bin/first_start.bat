a215064e526386189ee6bad96054c29be6b8305e
Cristian <cristianporras.v@gmail.com>
2022-03-30 16:57:39 -0300

    Trabajo en grupo-actualizacion de script first

--- bin/first_start.bat
+++ bin/first_start.bat
@@ -1,4 +1,4 @@
 
-docker-compose exec app composer update
+docker-compose exec app composer install
 docker-compose exec app chmod 777 ./web/assets -R
 docker-compose exec app chmod 777 ./runtime -R
